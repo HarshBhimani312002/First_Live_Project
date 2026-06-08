f30ae4e63e4ad7d0228e0ea5d57fd3e542cc40a7
Harsh Bhimani <bhimaniharsh31@gmail.com>
2026-06-08 14:52:02 +0530

    Add content folder and test file

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import netlifyIdentity from "netlify-identity-widget";
+
+netlifyIdentity.init();
+
+window.addEventListener("load", () => {
+  if (window.location.hash.includes("recovery_token")) {
+    netlifyIdentity.open();
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
